7f4547ea18c042974577bf0fa1c5cf1c9b77edcf
Carmen Fraga <carmenfragad@gmail.com>
2022-06-01 00:19:52 +0200

    dockerfile and messages endpoint

---
+++ exercices/exercise01/messageapp/routes/index.routes.js
@@ -0,0 +1,16 @@
+const messagesService = require("../services/messages.service");
+const router = require("express").Router();
+
+
+router.post('/messages', (req, res, next) => {
+
+  const { destination, body } = req.body
+
+  messagesService
+    .sendMessage({ destination, body })
+    .then((message) => res.status(200).json(message.data))
+    .catch(err => res.status(500).json({ message: "this message has not been sent" }))
+})
+
+
+module.exports = router;
